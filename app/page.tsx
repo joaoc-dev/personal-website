4fbe4a8f96e204b547bfdd545ced63c371527ecd
joaoc-dev <contact@joao-carvalho.dev>
2025-08-10 21:25:50 +0100

    feat: add light/dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { ThemeToggle } from '@/components/ui/theme-toggle';
+
 export default function Home() {
   return (
-    <div>Welcome to my home page!</div>
+    <>
+      <div>Welcome to my home page!</div>
+      <ThemeToggle />
+    </>
   );
 }
---
+++ components/ui/theme-toggle.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
+import * as React from 'react';
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  const handleThemeToggle = () => {
+    console.warn('theme', theme);
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+  return (
+    <button
+      type="button"
+      onClick={handleThemeToggle}
+      className="icon-button"
+    >
+      <Sun className="h-6 w-6 rotate-0 scale-100 transition-all duration-500 dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all duration-500 dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </button>
+  );
+}
